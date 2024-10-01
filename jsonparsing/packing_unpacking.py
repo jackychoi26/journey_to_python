285fa9ca16cb50ad3a6f44efcf15ec63a76e81e9
jackychoi26 <gmu1233@gmail.com>
2024-10-01 17:16:19 +0800

    Update README and add packing_unpacking

---
+++ jsonparsing/packing_unpacking.py
@@ -0,0 +1,25 @@
+"""
+In Python, packing refers to the process of putting values in a new tuple or list together.
+Each value is separated by a comma.
+"""
+
+number1 = 1
+number2 = 2
+number3 = 3
+
+# Tuple
+tuple = (number1, number2, number3)
+
+# List
+list = [number1, number2, number3]
+
+"""
+* (Asterisk) will pack the parameters into a tuple or list.
+- * is used also for both power and multiplication. 
+"""
+def sumOf(*nums):
+    print(f"Our numbers are {nums}")
+    return sum(nums)
+
+sumA = sumOf(number1, number2, number3) # Our numbers are (1, 2, 3)
+print(sumA) # 6
